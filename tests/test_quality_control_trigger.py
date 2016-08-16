e5ce42a024fd3ae95c725dd52bd1a80d8c4b2cdb
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:12 +0200

    Upgrade test

--- tests/test_quality_control_trigger.py
+++ tests/test_quality_control_trigger.py
@@ -3,22 +3,12 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('quality_control_trigger')
-
-    def test0005views(self):
-        'Test views'
-        test_view('quality_control_trigger')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'quality_control_trigger'
 
 
 def suite():
